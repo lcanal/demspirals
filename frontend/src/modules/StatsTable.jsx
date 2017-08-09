ee0d66c9a3da7bb9bfe9fba1d6cc5870fb8db999
Luis Canales <lacanales@ucdavis.edu>
2017-08-09 11:57:07 -0700

    Using new table component, trying to fix setstate issues.

--- frontend/src/modules/StatsTable.jsx
+++ frontend/src/modules/StatsTable.jsx
@@ -11,6 +11,7 @@ const selectRowProp = {
 
 
 class StatTable extends Component {
+    
     state = {
         players: [],
         playerHeaders: [],
@@ -18,6 +19,10 @@ class StatTable extends Component {
         loadState: 0.0,
         numLimit: 0
     }
+    
+    options = {
+      sortIndicator: true 
+    };
 
     async componentDidMount(){
         const response  = await fetch(this.props.apiURL)
@@ -55,12 +60,13 @@ class StatTable extends Component {
 
             //Actually grab data in headers
             for (var pidx in stats) {
+                var statkey
                    if (stats.hasOwnProperty(pidx)) {
                        if (stats[pidx].leaguename.length > 0){
-                            var statkey = stats[pidx].leaguename
-                            player[statkey] = stats[pidx].value
+                            statkey = stats[pidx].leaguename
+                            player[statkey] = stats[pidx].value.toFixed(2)
                        }else{
-                            var statkey = stats[pidx].name
+                            statkey = stats[pidx].name
                             player[statkey] = stats[pidx].statnum
                        }
                    }
@@ -81,32 +87,60 @@ class StatTable extends Component {
         })
     }
 
+    componentWillUnmount () {
+     //Trying to see why I get setstate() errors only on refresh.
+     //Async set state: 
+     //https://medium.com/front-end-hacking/async-await-with-react-lifecycle-methods-802e7760d802
+    }
+
     render(){
         var headers = []
-        headers.push(<TableHeaderColumn isKey={true} dataField="id">#</TableHeaderColumn>)
-        headers.push(<TableHeaderColumn dataField="name">Player</TableHeaderColumn>)
+        headers.push(<TableHeaderColumn key="id" isKey={true} dataField="id" hidden={true}>#</TableHeaderColumn>)
+        headers.push(<TableHeaderColumn key="name" dataField="name">Player</TableHeaderColumn>)
        
-        headers.push(<TableHeaderColumn dataField="position">Position</TableHeaderColumn>)
-        headers.push(<TableHeaderColumn dataField="teamname">Team</TableHeaderColumn>)
+        headers.push(<TableHeaderColumn key="position" dataField="position">Position</TableHeaderColumn>)
+        headers.push(<TableHeaderColumn key="teamname" dataField="teamname">Team</TableHeaderColumn>)
         //Build headers as we get them from the api
         this.state.playerHeaders.forEach(function(header) {
-            headers.push(<TableHeaderColumn dataField={header}>{header}</TableHeaderColumn>)
+            headers.push(<TableHeaderColumn key={header} dataField={header} dataSort={true} caretRender={getCaret}>{header}</TableHeaderColumn>)
             
         }, this);
-        headers.push(<TableHeaderColumn dataField="TotalPoints">Total Points</TableHeaderColumn>)
+        headers.push(<TableHeaderColumn key="totalfantasypoints" dataField="totalfantasypoints" dataSort caretRender={getCaret}>Total Points</TableHeaderColumn>)
         return (
             <div>
             <Fade in={!this.state.showTable} unmountOnExit={true} >
                 <ProgressBar className="stats-table-load-status" now={this.state.loadState} max={this.state.numLimit} />
             </Fade>
             <Fade in={this.state.showTable} transitionAppear={true} >
-            <BootstrapTable className="stats-table" selectRow={ selectRowProp } data={this.state.players} condensed hover bordered responsive version='4'>
+                <div>
+                <BootstrapTable selectRow={ selectRowProp } 
+                                data={this.state.players} 
+                                options={this.options}
+                                multiColumnSort={ 2 }
+                                pagination condensed bordered hover responsive version='4'>
                     {headers}
-            </BootstrapTable>
+                </BootstrapTable>
+                </div>
             </Fade>
             </div>
         )
     }
+}
+
+function getCaret(direction) {
+    if (direction === 'asc') {
+        return (
+        <span><strong> ^</strong></span>
+        );
+    }
+    if (direction === 'desc') {
+        return (
+        <span><strong> v</strong></span>
+        );
+    }
+  return (
+    <span></span>
+  );
 }
 
 export default StatTable;
