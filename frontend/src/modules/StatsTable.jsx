c8c16f9c5e5f9bc879a1bd7423655ef401696718
Luis Canales <cluchomucho@gmail.com>
2017-08-09 08:04:09 -0700

    Got big stats working

--- frontend/src/modules/StatsTable.jsx
+++ frontend/src/modules/StatsTable.jsx
@@ -1,118 +1,112 @@
 import React, { Component } from 'react';
-import { Table,Fade,ProgressBar } from 'react-bootstrap';
+import { Fade,ProgressBar } from 'react-bootstrap';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import '../css/react-bootstrap-table-all.min.css';
 import "../css/StatsTable.css";
+
+const selectRowProp = {
+  mode: 'checkbox',
+  clickToSelect: true  // enable click to select
+};
+
 
 class StatTable extends Component {
     state = {
         players: [],
         playerHeaders: [],
-        rows: [],
         showTable: false,
         loadState: 0.0,
-        numLimit: 20
+        numLimit: 0
     }
 
     async componentDidMount(){
         const response  = await fetch(this.props.apiURL)
         const json      = await response.json()
         var playerHeaders = []
-        var rows = []; 
+        var players = []
+
         var headerset = false;
         var position = this.props.position
 
+        this.setState({
+            "numLimit" : json.playerdata.length
+        });
+
         for (var index = 0; index < json.playerdata.length; index++) {
-            if (index > this.state.numLimit) {
-                break;
-            }
             var player = json.playerdata[index]
-            player.name = <strong>{player.firstname} {player.lastname}</strong>
-            
-            //console.log("Fetching: http://localhost:8080/api/player/"+position+"/"+player.id)
+            player.name = player.firstname + " " + player.lastname
+
             var playerstatsresponse =  await fetch("/api/player/"+position+"/"+player.id)
             var stats = await playerstatsresponse.json()
-            rows.push(<ResultEntry player={player} key={player.id} pid={player.id} playerstats={stats} />)
 
+            //Set once for headers.
             if (!headerset){
-               for (var idx in stats) {
-                   if (stats.hasOwnProperty(idx)) {
-                       if (stats[idx].leaguename.length > 0){
-                            playerHeaders.push(stats[idx].leaguename)
+               for (var hidx in stats) {
+                   if (stats.hasOwnProperty(hidx)) {
+                       if (stats[hidx].leaguename.length > 0){
+                            playerHeaders.push(stats[hidx].leaguename)
                        }else{
-                           playerHeaders.push(stats[idx].name)
+                            playerHeaders.push(stats[hidx].name)
                        }
                    }
                }
                headerset = true;
             }
 
+            //Actually grab data in headers
+            for (var pidx in stats) {
+                   if (stats.hasOwnProperty(pidx)) {
+                       if (stats[pidx].leaguename.length > 0){
+                            var statkey = stats[pidx].leaguename
+                            player[statkey] = stats[pidx].value
+                       }else{
+                            var statkey = stats[pidx].name
+                            player[statkey] = stats[pidx].statnum
+                       }
+                   }
+               }
+
+
             this.setState({
                 "loadState": index
             })
+
+            players.push(player)
         }
        
-        
         this.setState({
-            "players" : json.playerdata,
+            "players" : players,
             "playerHeaders" : playerHeaders,
-            "rows" : rows,
             "showTable": true
         })
     }
 
     render(){
         var headers = []
-        headers.push(<th key="Player">Player</th>)
-        headers.push(<th key="Position">Position</th>)
-        headers.push(<th key="Team">Team</th>)
+        headers.push(<TableHeaderColumn isKey={true} dataField="id">#</TableHeaderColumn>)
+        headers.push(<TableHeaderColumn dataField="name">Player</TableHeaderColumn>)
+       
+        headers.push(<TableHeaderColumn dataField="position">Position</TableHeaderColumn>)
+        headers.push(<TableHeaderColumn dataField="teamname">Team</TableHeaderColumn>)
         //Build headers as we get them from the api
         this.state.playerHeaders.forEach(function(header) {
-            headers.push(<th key={header}>{header}</th>)
+            headers.push(<TableHeaderColumn dataField={header}>{header}</TableHeaderColumn>)
+            
         }, this);
-        headers.push(<th key="TotalPoints">Total Points</th>)
+        headers.push(<TableHeaderColumn dataField="TotalPoints">Total Points</TableHeaderColumn>)
         return (
             <div>
             <Fade in={!this.state.showTable} unmountOnExit={true} >
                 <ProgressBar className="stats-table-load-status" now={this.state.loadState} max={this.state.numLimit} />
             </Fade>
             <Fade in={this.state.showTable} transitionAppear={true} >
-            <Table className="stats-table"  condensed hover bordered responsive >
-                <thead><tr>
+            <BootstrapTable className="stats-table" selectRow={ selectRowProp } data={this.state.players} condensed hover bordered responsive version='4'>
                     {headers}
-                </tr></thead>
-                <tbody>
-                    {this.state.rows}
-                </tbody>
-            </Table>
+            </BootstrapTable>
             </Fade>
             </div>
         )
     }
-}
-
-
-class ResultEntry extends Component {
-  render(){
-      var statsTD = []
-      var stats = this.props.playerstats
-      for (var index in stats) {
-          if (stats.hasOwnProperty(index)) {
-             if (stats[index].leaguename.length > 0) {
-                statsTD.push(<td key={stats[index].playerid+"-"+stats[index].name}>{stats[index].value.toFixed(2)}</td>)
-             }else{
-                statsTD.push(<td key={stats[index].playerid+"-"+stats[index].name}>{stats[index].statnum.toFixed(2)}</td>)
-             }
-          }
-      }
-    return(
-      <tr className="stat-row" key={this.props.pid}>
-        <td>{this.props.player.name}</td>
-        <td>{this.props.player.position}</td>
-        <td>{this.props.player.teamname} </td>
-        {statsTD}
-        <td>{this.props.player.totalfantasypoints.toFixed(2)}</td>
-      </tr>
-    )
-  }
 }
 
 export default StatTable;
