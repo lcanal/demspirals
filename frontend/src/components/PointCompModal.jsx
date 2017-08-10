14d4658feb46443f1d35c0535452051b443c0033
Luis A Canales <lacanales@ucdavis.edu>
2017-08-10 05:55:00 -0700

    First shot at chart.

--- frontend/src/components/PointCompModal.jsx
+++ frontend/src/components/PointCompModal.jsx
@@ -1,5 +1,7 @@
 import React, {Component } from 'react';
 import {Table,Button,Modal} from 'react-bootstrap';
+import { Bar } from 'react-chartjs-2';
+
 import "../css/Modals.css";
 
 class PointCompModal extends Component {
@@ -31,10 +33,40 @@ class PointCompModal extends Component {
           )
       }
     }
+
+    var options = {
+      responsive: true,
+      maintainAspectRatio: false,
+      stacked: true,
+      spanGaps: true,
+      lineTension: 0.1,
+      scales: {
+            yAxes: [{
+                stacked: true
+            }]
+        }
+    }
+
+    var data = {
+      labels: ["1","2","3","4"],
+      dataSets: [
+        {
+          label: "Age",
+          data: [10,3,5,10],
+          backgroundColor: generateColors([10,3,5,10],'rgba(75, 192, 192, 0.2)')
+        },
+        {
+        label: "Age2",
+          data: [12,9,7,11],
+          backgroundColor: generateColors([10,3,5,10],'rgba(255, 99, 132, 0.2)')
+        }
+      ]
+    }// data
+
     return (
       <Modal {...this.props} bsSize="large" dialogClassName="custom-modal" aria-labelledby="contained-modal-title-lg">
         <Modal.Header  closeButton>
-          <Modal.Title id="contained-modal-title-lg">Modal heading</Modal.Title>
+          <Modal.Title id="contained-modal-title-lg">Player Point Composition</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Table>
@@ -42,8 +74,9 @@ class PointCompModal extends Component {
               <th><strong>Player</strong></th>
               <th><strong>Point Composition</strong></th>
             </tr></thead>
-            <tbody>{playas}</tbody>
+            <tbody></tbody>
           </Table>
+          <Bar data={data} options={options} height={200} />
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={this.props.onHide}>Close</Button>
@@ -53,5 +86,18 @@ class PointCompModal extends Component {
   }
 }
 
+function generateColors(dataArray,colorString){
+  var colorArray = [colorString];
+
+  if(typeof dataArray === "undefined"){
+    return colorArray.push(colorString);
+  }
+
+  for (var i = 0; i < dataArray.length; i++) {
+    colorArray.push(colorString);
+  }
+
+  return colorArray;
+}
 
 export default PointCompModal;
